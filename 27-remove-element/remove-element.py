4f0f032f23848475fece37d5c30217b0a21f89cc
udaykarthik4245 <155714405+udaykarthik4245@users.noreply.github.com>
2024-01-23 18:03:21 +0530

    Time: 33 ms (89.19%) | Memory: 16.6 MB (55.83%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k
+                # # Initialize a pointer for the current position
+        # current = 0
+
+        # # Iterate through the list
+        # while current < len(nums):
+        #     if nums[current] == val:
+        #         # If the current element is equal to val, remove it
+        #         nums.pop(current)
+        #     else:
+        #         # If the current element is not equal to val, move to the next element
+        #         current += 1
+
+        # # Return the new length of the list
+        # return len(nums)
+
