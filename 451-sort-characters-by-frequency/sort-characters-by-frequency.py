c67cf732108fcb7d66851c0b4e4b10f44bb8cb29
udaykarthik4245 <155714405+udaykarthik4245@users.noreply.github.com>
2024-02-07 09:36:45 +0530

    Time: 66 ms (19.17%) | Memory: 21.3 MB (13.93%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -24,3 +24,16 @@ class Solution:
             r.extend([char]*count)
             print(f"{char} - {count}")
         return(r)
+                    #                               small solution
+# class Solution:
+#     def frequencySort(self, s: str) -> str:
+#         count = Counter(s)
+#         buckets = ["" for _ in range(len(s)+1)]
+
+#         for c, f in count.items():
+#             buckets[f] += (c*f)
+        
+#         return "".join(buckets[::-1])
+
+
+        
