1afa7fe97752fc694aa2d39fc165b533abeb3c7c
udaykarthik4245 <155714405+udaykarthik4245@users.noreply.github.com>
2024-02-08 22:31:46 +0530

    Time: 2035 ms (75.72%) | Memory: 16.5 MB (92.57%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -15,4 +15,18 @@ class Solution:
             dp[i] = min_squares + 1
         
         return dp[n]
-
+class Solution:
+    def numSquares(self, n: int) -> int:
+        if n == 0: return 0
+        if n == 1: return 1
+        steps = [0 for _ in range(n+1)]
+        steps[1] = 1
+        
+        for i in range(2,n+1):
+            j = 1
+            nums = []
+            while j**2 <= i:
+                nums.append(i - j**2)
+                j += 1
+            steps[i] = min([steps[num]+1 for num in nums])
+        return steps[-1]
