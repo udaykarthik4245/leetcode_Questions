956bdfab64325e34d2b82f293d2fbf3251bd640f
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-04-11 18:33:09 +0530

    Time: 125 ms (41.32%) | Memory: 16.8 MB (11.49%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
@@ -2,25 +2,6 @@ class Solution:
     def makeSmallestPalindrome(self, s: str) -> str:
         
         s=list(s)
-        # print(l)
-        # first=0
-        # last=len(l)-1
-        # while first < last:
-        #     if l==set[l]:
-        #         if l[first] != l[last]:
-        #             # Replace the character at last index with the character at first index
-        #             l[first]=l[last]
-        #             # l[last] = l[first]
-        #     else:
-        #         if l[first] != l[last]:
-        #             l[last] = l[first]
-        #         # Move the pointers inward
-        #     first += 1
-        #     last -= 1
-        
-        # # Join the modified list to form the smallest palindrome
-        # return ''.join(l)
-        # # return l
         n=len(s)
         for i in range(n//2):
             if(s[i]!=s[n-i-1]):
