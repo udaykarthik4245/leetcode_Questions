6372a19525b52bef5b4d80675c14c5dda6a84f5a
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-04-11 22:58:49 +0530

    Time: 161 ms (42.43%) | Memory: 17.7 MB (98.63%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.py
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -14,3 +14,10 @@ class Solution:
             if arr[j]<=arr[j+1]:
                 return False
         return True
+
+        # another solution
+
+        i, j, n = 0, len(A) - 1, len(A)
+        while i + 1 < n and A[i] < A[i + 1]: i += 1
+        while j > 0 and A[j - 1] > A[j]: j -= 1
+        return 0 < i == j < n - 1
