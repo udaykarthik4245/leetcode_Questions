7f2b484c369bbaad89bdf0341bbe8d9249d9fc77
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-04-11 18:32:59 +0530

    Time: 127 ms (38.20%) | Memory: 16.8 MB (40.79%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
@@ -27,5 +27,21 @@ class Solution:
                 c=min(s[i],s[n-i-1])
                 s[i],s[n-i-1]=c,c
         return ''.join(s)
+        # another solution
+        i = 0
+        j = len(s) - 1
+        s = list(s)
+
+        while i <= j:
+            if s[i] != s[j]:
+                if s[i] < s[j]:
+                    s[j] = s[i]
+                else:
+                    s[i] = s[j]
+
+            i += 1
+            j -= 1
+        
+        return "".join(s)
         
         
