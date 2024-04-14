4c33046a9d55d6cd90f56baa7653e94a3a400e2f
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-04-14 17:17:48 +0530

    Time: 48 ms (96.87%) | Memory: 16.6 MB (77.41%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,4 +1,24 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         maxi=3**20
-        return n>0 and  maxi%n==0#n^(n-1) ==1
+
+
+        return n>0 and  maxi%n==0
+        #n^(n-1) ==1
+    # recursion approach
+        if n == 0:
+            return False
+        if n%3 == 0:
+            return self.isPowerOfThree(n//3)
+        if n == 1:
+            return True
+        else:
+            return False
+    # brute force approach
+        if n <= 0:  
+            return False
+        while n % 3 == 0:  
+            n /= 3
+        return n == 1
+        if n ==1:
+            return True
