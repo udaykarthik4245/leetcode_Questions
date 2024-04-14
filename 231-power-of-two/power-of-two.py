1f88610c4577c69767bb8e05de637d19153bfdfd
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-04-14 17:03:10 +0530

    Time: 43 ms (15.13%) | Memory: 16.7 MB (10.23%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -6,3 +6,16 @@ class Solution:
             return False
         else:
             return n & (n-1)==0
+        
+
+        return n>0 and log(n,2)%1==0
+        # brute force approach
+        i = 0
+        while 2**i <= n:
+            if 2**i == n:
+                return True
+            else:
+                i += 1
+        return False 
+        # using xor and operation
+        return n != 0 and n == (n^(n-1)) & n
