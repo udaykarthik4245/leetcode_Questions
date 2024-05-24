c2d9144a242478876759e3c5ca7571340e38ac04
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-05-25 00:10:18 +0530

    Time: 26 ms (95.32%) | Memory: 16.5 MB (30.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -6,14 +6,16 @@ class Solution:
 
         if n==1: return 1
         if n==2: return 2
-        # tb=1
-        # ob=2
-        # for i in range(n-2):
-        #     corr=tb+ob
-        #     tb=ob
-        #     ob=corr
-        # return corr
-        # o(2^n)
-        two=self.climbStairs(n-2)
-        one=self.climbStairs(n-1)
-        return one+two
+        tb=1
+        ob=2
+        for i in range(n-2):
+            corr=tb+ob
+            tb=ob
+            ob=corr
+        return corr
+
+
+        # # o(2^n) recursion
+        # two=self.climbStairs(n-2)
+        # one=self.climbStairs(n-1)
+        # return one+two
