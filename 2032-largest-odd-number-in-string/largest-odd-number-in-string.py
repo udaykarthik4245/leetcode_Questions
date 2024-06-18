ac8994e3dcb140b5bbf7c214f632a3ee9f0f270d
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2024-06-18 11:53:30 +0530

    Time: 136 ms (9.69%) | Memory: 26.7 MB (5.01%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -13,5 +13,12 @@ class Solution:
         
         # No odd digit found
         return ""
+        # 2nd solution
+
+
+        # for i in range(len(num)-1,-1,-1):
+        #     if int(num[i])%2!=0:
+        #         return num[:i+1]
+        # return ''
             
         
