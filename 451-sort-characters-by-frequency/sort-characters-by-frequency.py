ed60ef6eec6d38fc4f5ab720ad794fa67c65ee8d
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2025-07-29 02:50:39 +0530

    Time: 11 ms (49.78%) | Memory: 18.8 MB (79.36%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,29 +1,22 @@
 from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
-        l=list(s)
-        a=[]
-        i=0
-        j=1
-        a.append(l[0])
-        for i in range(j,len(l)):
-            if l[i]!=[j]:
-
-                a.append(l[i])
-                j+=1
-            # if l[i]==l[j]:
+        d={}
+        for i in s:
+            if i not in d:
+                d[i]=1 
             else:
-                a.append(l[i])
-                a.append(l[j])
-        # a.sort(reverse=True)
-        # return a[::-1]
-        a_count=Counter(a)
-        sor_a=sorted(a_count.items(),key=lambda x:x[1],reverse=True)
-        r=[]
-        for char,count in sor_a:
-            r.extend([char]*count)
-            print(f"{char} - {count}")
-        return(r)
+                d[i]+=1 
+        print(d)
+        print()
+        ss=""
+        res=dict(sorted(d.items(),key=lambda i: i[1],reverse=True))
+        for i,j in res.items():
+            ss=ss+(i*j)  
+        print(ss)
+        print(res)
+        return ss
+            
                     #                               small solution
 # class Solution:
 #     def frequencySort(self, s: str) -> str:
