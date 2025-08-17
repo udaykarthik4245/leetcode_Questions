e316bae38c48ac12ef7e1a4ae6ffb4828c6516f4
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2025-08-17 23:41:58 +0530

    Time: 23 ms (72.90%) | Memory: 18.4 MB (29.68%) - LeetSync

--- 867-new-21-game/new-21-game.py
+++ 867-new-21-game/new-21-game.py
@@ -14,4 +14,17 @@ class Solution:
                 wsum-=dp[l]
                 l+=1
         return prob 
+      #2nd solution  
+# class Solution:
+#     def new21Game(self, n: int, k: int, maxPts: int) -> float:
+#         if k == 0 or n >= k + maxPts: return 1
+#         dp = [1.0] + [0.0] * n
+#         curr = 1
+#         for i in range(1, n+1):
+#             dp[i] = curr / maxPts
+#             if i < k:
+#                 curr += dp[i]
+#             if i >= maxPts:
+#                 curr -= dp[i - maxPts]
         
+#         return sum(dp[k:])
