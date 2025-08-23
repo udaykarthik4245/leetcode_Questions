031b992eca85be4529364c6ab5015192bf0942d5
uday karthik pillarisetty <155714405+udaykarthik4245@users.noreply.github.com>
2025-08-23 19:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (50.63%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,4 @@
-from collections import Counter
+# from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
     #main optimal solution 
@@ -12,30 +12,3 @@ class Solution:
             count += 1 if candidate == num else -1
         return candidate
     
-    #solution using sort and comparing 
-
-            # n=len(nums)
-        # major=n//2
-
-        # nums.sort()
-        # count=1
-        # for i in range(1,n):
-        #     if nums[i]==nums[i-1]:
-        #         count+=1
-        #         if count>major:
-        #             return nums[i]
-        #     else:
-        #         count=1
-        # if n==1:
-        #     return nums[0]
-
-        # return -1
-
-        # 2nd solution using hashmap
-        # c=Counter(nums)
-        # for i in c:
-        #     if c[i]>major:
-        #         return i
-        # return -1
-
-        
